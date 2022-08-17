9453bbd90b35a1b6baba2c48b71d61712a72f4ea
MinhNamThan <thanminhnam17022001@gmail.com>
2022-08-18 01:49:38 +0700

    login and role

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,19 +1,24 @@
 module SessionsHelper
-  def log_in object
-    session[:object_id] = object.id
+  def log_in user
+    session[:user_id] = user.id
   end
 
-  def current_object
-    @current_object ||= Student.find_by id: session[:object_id]
-    @current_object ||= Admin.find_by id: session[:object_id]
+  def current_user
+    @current_user ||= User.find_by id: session[:user_id]
+  end
+
+  def admin?
+    user = current_user
+    user.role != 2
   end
 
   def logged_in?
-    !current_object.nil?
+    current_user.present?
   end
 
   def log_out
-    session.delete :object_id
-    @current_object = nil
+    session.delete :user_id
+
+    @current_user = nil
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'users/new'
+  get 'users/show'
+  get 'users/new'
   get 'users/create'
   get 'users/destroy'
   get 'sessions/new'
@@ -12,4 +14,10 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete '/logout', to: 'sessions#destroy'
+
+  resources :users
+  namespace :admin do
+    root to: "static_pages#index"
+    resources :static_pages
+  end
 end
