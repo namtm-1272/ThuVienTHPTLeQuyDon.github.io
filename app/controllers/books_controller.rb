5c307ded5f3dfc6db8619ab26bc5ad581dcf888b
hanhnd <hanhnd156@gmail.com>
2022-08-18 15:32:51 +0700

    add faker seed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,9 @@
 class BooksController < ApplicationController
-    BOOKS_PER_PAGE = 4
+    BOOKS_PER_PAGE = 8
 
     def show
         @page = [params.fetch(:page, 1).to_i, 1].max
-        @totalPage = (Book.all.length() * 1.0 / BOOKS_PER_PAGE).ceil
+        @totalPage = (Book.all.count * 1.0 / BOOKS_PER_PAGE).ceil
         @books = Book.offset((@page - 1) * BOOKS_PER_PAGE).limit(BOOKS_PER_PAGE)
     end
 
