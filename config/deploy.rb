6f9ce95be0b16dcfce0dae70753da682a42099fc
MinhNamThan <thanminhnam17022001@gmail.com>
2022-08-25 06:02:23 +0700

    add link

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.17.1"
 
 set :application, "iorder_server"
-set :repo_url, "git@github.com:namtm-1272/thuvienthptlequydon.github.io.git"
+set :repo_url, "git@github.com:MinhNamThan/ThuVienTHPTLeQuyDon.github.io.git"
 set :pty, true
 set :linked_files, %w(config/database.yml config/application.yml)
 set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads)
