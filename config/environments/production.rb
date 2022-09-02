573c0285ee77082b8e2b29eff7eff9cab62674eb
MinhNamThan <thanminhnam17022001@gmail.com>
2022-09-02 23:42:59 +0700

    add forget password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,4 +118,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {host: ENV["host"]}
+  config.action_mailer.smtp_settings = {
+    domain: "mail.google.com",
+    user_name: ENV["mail_user_name"],
+    password: ENV["mail_user_password"],
+    address: ENV["mail_address"],
+    port: ENV["mail_port"],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
