e00192744b9a1f387a788493bedec16e729f694d
MinhNamThan <thanminhnam17022001@gmail.com>
2022-09-03 18:13:14 +0700

    change default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,7 +119,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {host: ENV["host"]}
+  config.action_mailer.default_url_options = {host: "103.166.185.90"}
   config.action_mailer.smtp_settings = {
     domain: "mail.google.com",
     user_name: ENV["mail_user_name"],
@@ -129,5 +129,4 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true
   }
-  Rails.application.routes.default_url_options[:host] = '103.166.185.90'
 end
